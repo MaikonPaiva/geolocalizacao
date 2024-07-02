8a342c5c9d4c9c417daf9b6f7f3b010d22ec6a60
Maikon Paiva <maikonpaiva.dev@gmail.com>
2024-07-02 10:38:02 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,35 @@
 let h2= document.querySelector('h2');
 
-function success(watchPosition){
-    h2.textContent = `Latitude: ${watchPosition.coords.latitude}, 
-    Longitude:${watchPosition.coords.longitude}`
+var map;
+
+function success(pos){
+    h2.textContent = `Latitude: ${pos.coords.latitude
+    }, 
+    Longitude:${pos.coords.longitude}`
+     if(map === undefined){
+            map = L.map('mapid').setView([
+            pos.coords.latitude,
+            pos.coords.longitude]
+            , 12);
+     } else {
+        map.remove();
+            map = L.map('mapid').setView([
+            pos.coords.latitude,
+            pos.coords.longitude]
+           , 12);
+     }
+
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    L.marker([
+        pos.coords.latitude,
+        pos.coords.longitude
+    ]).addTo(map)
+    .bindPopup('EU ESTOU AQUI!')
+    .openPopup();
 }
 
 function error(error){
@@ -12,6 +39,9 @@ function error(error){
 
 var watchID = navigator.geolocation.watchPosition(success, error, {
     enableHighAccuracy: true,
-    timeout: 10000,
+    timeout: 5000,
 });
+
+
 //navigator.geolocation.clearWatch(watchID);
+
